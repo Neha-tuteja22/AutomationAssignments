362b69e26dfd755451812c88714e805ec0503109
Neha Tuteja <ntuteja56@gmail.com>
2024-12-20 23:17:24 +0530

    Assignment 1 - Data types

---
+++ src/com/creatio/crm/language/basics/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.creatio.crm.language.basics;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	
+		System.out.print("Hello world");
+		System.out.format("Temperature of a city in degree Celsius: 25.5");
+
+	}
+
+}

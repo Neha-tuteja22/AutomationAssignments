277fc5e82b5b82392ffb9c88a7160f52f7a1936a
Neha Tuteja <ntuteja56@gmail.com>
2024-12-24 23:19:23 +0530

    AssignmentDataTypes

--- src/com/creatio/crm/language/basics/AssignmentArrays.java
+++ src/com/creatio/crm/language/basics/AssignmentArrays.java
@@ -3,7 +3,35 @@ package com.creatio.crm.language.basics;
 public class AssignmentArrays {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String [][][][] groceries = new String [5][5][5][5];
+		String beverage;
+		String Snacks;
+		String Dairyproducts;
+		String Produce;
+		groceries [0][0][0][0] beverage = "Orange juice";
+		groceries [1][1][1][1] beverage = "lemonade";
+		groceries [2][2][2][2] beverage ="Black coffee";
+		groceries [3][3][3][3] beverage = "Cola";
+		groceries [4][4][4][4] beverage = "Green tea";
+		
+		groceries [0][0][0][0] Snacks = "Popcorn";
+		groceries [1][1][1][1] Snacks = "Trail mix";
+		groceries [2][2][2][2] Snacks = "Beef jerky";
+		groceries [3][3][3][3] Snacks = "Salted nuts";
+		groceries [4][4][4][4] Snacks = "Rice cakes";
+		
+		groceries [0][0][0][0] Dairyproducts = "Cottage cheese";
+		groceries [1][1][1][1] Dairyproducts = "Heavy cream";
+		groceries [2][2][2][2] Dairyproducts = "Ice cream";
+		groceries [3][3][3][3] Dairyproducts = "Parmesan Cheese";
+		groceries [4][4][4][4] Dairyproducts = "Eggnog";
+		
+		groceries [0][0][0][0] Produce = "Apples";
+		groceries [1][1][1][1] Produce = "Bananas";
+		groceries [2][2][2][2] Produce = "Carrots";
+		groceries [3][3][3][3] Produce = "Potatoes";
+		groceries [4][4][4][4] Produce = "Onions";
 
 	}
 
---
+++ src/com/creatio/crm/language/basics/AssignmentDataType.java
@@ -0,0 +1,107 @@
+package com.creatio.crm.language.basics;
+
+public class AssignmentDataType {
+
+	public static void main(String[] args) {
+		
+		// using float to print temperature
+		float temperature = 25.5f;
+				System.out.println("Temperature of a city in degree celsius:" + temperature );
+		
+				// using boolean to print true or false statement
+					boolean order = true;
+					System.out.println("Whether a customer has placed an order: " + order);
+
+					// using int to print phone number
+					int phoneNumber = 123457890;
+					System.out.println("Person's phone number: " + phoneNumber);
+			
+					
+					// using float to display bank balance
+					float bankBalance = 1000.50f;
+					System.out.println("Amount of money in a customer's bank account:" + bankBalance);
+					
+					// using string to display email address
+					String email = "john.doe@example.com";
+					System.out.println("Person's email address:" + email);
+					
+					// using double to display latitude and longitude
+					float latitude = 37.7749f;
+					float longitude = -122.4194f;
+					System.out.println("coordinates of a location(latitude and longitude)" + latitude + longitude);
+					
+					// using boolean to display marital status
+					boolean maritalStatus = false;
+					System.out.println("Person's martial status:" + maritalStatus);
+					
+					// using String to display occupation
+					String occupation = "Software Engineer";
+					System.out.println("Person's occupation:" + occupation);
+					
+					// using short to display current year
+					short year = 2024;
+					System.out.println("Current year:" + year);
+					
+					// using string to display number of followers
+					String followers = "1,000,000";
+					System.out.println("Number of followers on social media platform: " + followers);
+					
+					// using string to print color
+					String color = "Blue";
+					System.out.println("Person's favorite color:" + color);
+					
+					// using float to print movie rating
+					float rating = 7.5f;
+					System.out.println("Rating of movie: " + rating);
+					
+					// using char to print blood type
+					char bloodType = 'A';
+					System.out.println("Person's blood type: " + bloodType);
+					
+					// using String to print book name
+					String book = "'To kill a mockingbird'";
+					System.out.println("Title of book: " + book);
+					
+					// using short to display number of employees
+					short empNumber = 500;
+					System.out.println("Number of employees: " + empNumber);
+					
+					// using string to display time
+					String time = "2:30 PM";
+					System.out.println("Time of an event: " + time);
+					
+					// using String to display country name
+					String country = "United States";
+					System.out.print("Name of a country: " + country);
+					
+					// using string to display eye color
+					String eyeColor = "Brown";
+					System.out.println("Person's eye color: " + eyeColor);
+					
+					// using string to display birthplace
+					String birthPlace = "New york city";
+					System.out.println("Person's birthplace: " + birthPlace);
+					
+					// using float to display distance
+					float distance = 200.5f;
+					System.out.println("Distance between two cities: " + distance);
+					
+					
+					
+							
+					
+					
+					
+					
+					
+					
+					
+					
+					
+		
+					
+					
+					
+	}
+
+}
